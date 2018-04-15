d32e263c319d93611c8d4a90a5b8838222d2cc4f
Coursal <coursal@espiv.net>
2018-04-15 23:18:48 +0300

    added and tested methods of form class

--- src/Form.java
+++ src/Form.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +6,104 @@ class Form
     private String name;
     private String reg_num;
     private int year_of_submission;
-    private List<Property> list = new ArrayList<Property>();
+    private List<Property> property_list = new ArrayList<Property>();
     private double enfia;
+    
+    
+    //constructors
+    public Form()
+    {
+        name=reg_num="NULL";
+        year_of_submission=-1;
+        enfia=-1;
+    }
+    
+    public Form(String n, String r, int yos)
+    {
+        name=n;
+        reg_num=r;
+        year_of_submission=yos;
+    }
+    
+    public void display()
+    {
+        System.out.println("****************Form Info****************");
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + reg_num);
+        System.out.println("Year of submission: " + year_of_submission);
+        System.out.println("ENFIA: " + enfia);
+        System.out.println();
+        for(Property listed_property:property_list)
+        {
+            listed_property.display();
+        }
+        System.out.println();
+        System.out.println("*****************************************");
+    }
+    
+    public void enfia_sum()
+    {
+        for(Property listed_property:property_list)
+            enfia+=listed_property.tax_calculation();
+    }
+    
+    public void add_property(Property p)
+    {
+        property_list.add(p);
+    }
+    
+    public void remove_property(Property p)
+    {
+        property_list.remove(p);
+    }
+    
+    public void clear_properties()
+    {
+        property_list.clear();
+    }
+    
+    
+    //getters and setters
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String n)
+    {
+        name=n;
+    }
+    
+    
+    public String getReg_num()
+    {
+        return reg_num;
+    }
+    
+    public void setReg_num(String r)
+    {
+        reg_num=r;
+    }
+    
+    
+    public int getYear_of_submission()
+    {
+        return year_of_submission;
+    }
+    
+    public void setYear_of_submission(int yos)
+    {
+        year_of_submission=yos;
+    }
+    
+    
+    public double getEnfia()
+    {
+        return enfia;
+    }
+    
+    public void setEnfia(double e)
+    {
+        enfia=e;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -3,12 +3,20 @@ public class main
     public static void main(String args[])
     {
         Residence r = new Residence("Keratsiniou 54", 45, 2013, true, 1);
-        r.display();
+        //r.display();
         
         Store s = new Store("Keratsiniou 54", 45, 2013, 5);
-        s.display();
+        //s.display();
         
         Field f = new Field("Keratsiniou 54", 45, 2013, false, true);
-        f.display();
+        //f.display();
+        
+        Form f1 = new Form("Mary Sue", "1111111111", 2018);
+        f1.add_property(r);
+        f1.add_property(s);
+        f1.add_property(f);
+        f1.enfia_sum();
+        f1.display();
+        
     }
 }
