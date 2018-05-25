20fac96009d8cace74000fe8eabf904d663bd5c1
Coursal <coursal@espiv.net>
2018-05-25 17:23:31 +0300

    added new panel for choosing form to modify, other minor changes

--- src/GUI.java
+++ src/GUI.java
@@ -3,6 +3,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Calendar;
 import javax.swing.*;
 
@@ -10,14 +11,17 @@ class GUI extends JFrame implements ActionListener
 {
     private JFrame frame;
     
+    ///////////////////////////////////
     private JPanel home_page;
     private JLabel home_title;
     private JButton insert;
     private JButton modify;
     private JButton cancel;
     private JButton info;
-    private JButton exit;
+    private JButton quit;
+    ///////////////////////////////////
     
+    ///////////////////////////////////
     private JPanel insert_page;
     private JLabel insert_title;
     private JLabel year_label;
@@ -27,20 +31,35 @@ class GUI extends JFrame implements ActionListener
     private JTextField reg_num_text;
     private JButton insert_button;
     private JButton insert_close;
+    ///////////////////////////////////
     
+    ///////////////////////////////////
     private JPanel modify_page;
     private JLabel modify_title;
+    private JLabel modify_list_label;
+    private JComboBox modify_forms;
+    private JButton modify_continue;
     private JButton modify_close;
     
+    private JPanel modify_form_page;
+    private JLabel modify_form_title;
+    private JButton modify_form_back;
+    private JButton modify_form_close;
+    ///////////////////////////////////
+    
+    ///////////////////////////////////
     private JPanel cancel_page;
     private JLabel cancel_title;
     private JButton cancel_close;
+    ///////////////////////////////////
     
+    ///////////////////////////////////
     private JPanel info_page;
     private JLabel info_title;
     private JButton info_close;
+    ///////////////////////////////////
     
-    GUI()
+    GUI(String[] form_list)
     {
         frame = new JFrame("ENFIApp Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -72,16 +91,16 @@ class GUI extends JFrame implements ActionListener
             info.setAlignmentX(CENTER_ALIGNMENT);
             info.setMaximumSize(new Dimension(Integer.MAX_VALUE, info.getMinimumSize().height));        //fill the width of the window
             
-            exit = new JButton("Exit");
-            exit.setAlignmentX(CENTER_ALIGNMENT);
-            exit.setMaximumSize(new Dimension(Integer.MAX_VALUE, exit.getMinimumSize().height));        //fill the width of the window
+            quit = new JButton("Quit");
+            quit.setAlignmentX(CENTER_ALIGNMENT);
+            quit.setMaximumSize(new Dimension(Integer.MAX_VALUE, quit.getMinimumSize().height));        //fill the width of the window
             
             
             insert.addActionListener(this);
             modify.addActionListener(this);
             cancel.addActionListener(this);
             info.addActionListener(this);
-            exit.addActionListener(this);
+            quit.addActionListener(this);
 
             
             home_page.add(home_title);
@@ -89,7 +108,7 @@ class GUI extends JFrame implements ActionListener
             home_page.add(modify);
             home_page.add(cancel);
             home_page.add(info);
-            home_page.add(exit);
+            home_page.add(quit);
        //////////////////////////////////////////////////////
        //////////////////////////////////////////////////////
        ////////////////////////////////////////////////////// 
@@ -130,7 +149,7 @@ class GUI extends JFrame implements ActionListener
             insert_button.setAlignmentX(CENTER_ALIGNMENT);
             insert_button.setMaximumSize(new Dimension(Integer.MAX_VALUE, insert_button.getMinimumSize().height));        //fill the width of the window
             
-            insert_close = new JButton("Back to Menu");
+            insert_close = new JButton("Exit to Menu");
             insert_close.setAlignmentX(CENTER_ALIGNMENT);
             insert_close.setMaximumSize(new Dimension(Integer.MAX_VALUE, insert_close.getMinimumSize().height));        //fill the width of the window
             insert_close.addActionListener(this);
@@ -160,14 +179,58 @@ class GUI extends JFrame implements ActionListener
             modify_title.setFont(new Font("Ubuntu", Font.BOLD, 16));
             modify_title.setAlignmentX(CENTER_ALIGNMENT);
             
-            modify_close = new JButton("Back to Menu");
+            modify_list_label = new JLabel("Choose Form", JLabel.CENTER);
+            modify_list_label.setAlignmentX(CENTER_ALIGNMENT);
+            modify_list_label.setMaximumSize(new Dimension(Integer.MAX_VALUE, modify_list_label.getMinimumSize().height)); 
+            
+            modify_forms = new JComboBox(form_list);
+            modify_forms.addActionListener(this);
+            
+            modify_continue = new JButton("Continue");
+            modify_continue.setAlignmentX(CENTER_ALIGNMENT);
+            modify_continue.setMaximumSize(new Dimension(Integer.MAX_VALUE, modify_continue.getMinimumSize().height));        //fill the width of the window
+            modify_continue.addActionListener(this);
+            
+            modify_close = new JButton("Exit to Menu");
             modify_close.setAlignmentX(CENTER_ALIGNMENT);
             modify_close.setMaximumSize(new Dimension(Integer.MAX_VALUE, modify_close.getMinimumSize().height));        //fill the width of the window
             modify_close.addActionListener(this);
             
             
             modify_page.add(modify_title);
+            modify_page.add(modify_list_label);
+            modify_page.add(modify_forms);
+            modify_page.add(modify_continue);
             modify_page.add(modify_close);
+       //////////////////////////////////////////////////////
+       //////////////////////////////////////////////////////
+       ////////////////////////////////////////////////////// 
+       
+       
+       //////////////////////////////////////////////////////
+       //////modify form page components initialization//////
+       ////////////////////////////////////////////////////// 
+        modify_form_page = new JPanel();
+        
+            modify_form_page.setLayout(new BoxLayout(modify_form_page, BoxLayout.Y_AXIS));
+            
+            modify_form_title = new JLabel("Modify Form Info", JLabel.CENTER);
+            modify_form_title.setFont(new Font("Ubuntu", Font.BOLD, 16));
+            modify_form_title.setAlignmentX(CENTER_ALIGNMENT);
+            
+            modify_form_back = new JButton("Back to Form Choosing");
+            modify_form_back.setAlignmentX(CENTER_ALIGNMENT);
+            modify_form_back.setMaximumSize(new Dimension(Integer.MAX_VALUE, modify_form_back.getMinimumSize().height));        //fill the width of the window
+            modify_form_back.addActionListener(this);
+            
+            modify_form_close = new JButton("Exit to Menu");
+            modify_form_close.setAlignmentX(CENTER_ALIGNMENT);
+            modify_form_close.setMaximumSize(new Dimension(Integer.MAX_VALUE, modify_form_close.getMinimumSize().height));        //fill the width of the window
+            modify_form_close.addActionListener(this);
+       
+        modify_form_page.add(modify_form_title);
+        modify_form_page.add(modify_form_back);
+        modify_form_page.add(modify_form_close);
        //////////////////////////////////////////////////////
        //////////////////////////////////////////////////////
        ////////////////////////////////////////////////////// 
@@ -184,7 +247,7 @@ class GUI extends JFrame implements ActionListener
             cancel_title.setFont(new Font("Ubuntu", Font.BOLD, 16));
             cancel_title.setAlignmentX(CENTER_ALIGNMENT);
             
-            cancel_close = new JButton("Back to Menu");
+            cancel_close = new JButton("Exit to Menu");
             cancel_close.setAlignmentX(CENTER_ALIGNMENT);
             cancel_close.setMaximumSize(new Dimension(Integer.MAX_VALUE, cancel_close.getMinimumSize().height));        //fill the width of the window
             cancel_close.addActionListener(this);
@@ -209,7 +272,7 @@ class GUI extends JFrame implements ActionListener
             info_title.setAlignmentX(CENTER_ALIGNMENT);
             
   
-            info_close = new JButton("Back to Menu");
+            info_close = new JButton("Exit to Menu");
             info_close.setAlignmentX(CENTER_ALIGNMENT);
             info_close.setMaximumSize(new Dimension(Integer.MAX_VALUE, info_close.getMinimumSize().height));        //fill the width of the window
             info_close.addActionListener(this);
@@ -291,7 +354,28 @@ class GUI extends JFrame implements ActionListener
             
             frame.pack();
         }
-        else if(e.getSource() == exit)
+        else if(e.getSource() == modify_continue)
+        {
+            frame.remove(modify_page);
+            frame.setContentPane(modify_form_page);
+            
+            frame.pack();
+        }
+        else if(e.getSource() == modify_form_back)
+        {
+            frame.remove(modify_form_page);
+            frame.setContentPane(modify_page);
+            
+            frame.pack();
+        }
+        else if(e.getSource() == modify_form_close)
+        {
+            frame.remove(modify_form_page);
+            frame.setContentPane(home_page);
+            
+            frame.pack();
+        }
+        else if(e.getSource() == quit)
         {
             frame.dispose();
         }
--- src/main.java
+++ src/main.java
@@ -1,25 +1,41 @@
+import java.util.ArrayList;
+
 public class main
 {
     public static void main(String args[])
     {
-        Residence r = new Residence("Telegrave Ave. 54", 45, 1999, true, 1);
+        Residence r1 = new Residence("Telegrave Ave. 54", 45, 1999, true, 1);
         //r.display();
-        
-        Store s = new Store("St. Charles Ave. 354", 60, 2005, 5);
+        Store s1 = new Store("St. Charles Ave. 354", 60, 2005, 5);
         //s.display();
-        
-        Field f = new Field("Huxley Sq. 3", 1000, 2000, false, true);
+        Field f1 = new Field("Huxley Sq. 3", 1000, 2000, false, true);
         //f.display();
         
-        Form f1 = new Form("Mary Sue", "1111111111", 2018);
-        f1.add_property(r);
-        f1.add_property(s);
-        f1.add_property(f);
+        Form mary = new Form("Mary Sue", "1111111111", 2018);
+        mary.add_property(r1);
+        mary.add_property(s1);
+        mary.add_property(f1);
+        mary.enfia_sum();
+        mary.display();
+        mary.backup();
         
-        f1.enfia_sum();
-        f1.display();
-        f1.backup();  
         
-        GUI gui_instance = new GUI();
+        Residence r2 = new Residence("Second Str. 35", 65, 2013, false, 0);
+        //r.display();
+        Store s2 = new Store("Huxley Sq. 15", 90, 2015, 2);
+        //s.display();
+        
+        Form jamal = new Form("Jamal Odom", "2222222222", 2018);
+        jamal.add_property(r2);
+        jamal.add_property(s2);
+        jamal.enfia_sum();
+        jamal.display();
+        jamal.backup();
+        
+        ArrayList<String> form_list = new ArrayList<String>();
+        form_list.add(mary.getName());
+        form_list.add(jamal.getName());
+
+        GUI gui_instance = new GUI(form_list.toArray(new String[0]));
     }
 }
